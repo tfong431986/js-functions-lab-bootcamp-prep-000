abedbe5748ec4f87b0c3d5d0f471b68f7a94c3c4
tfong4386 <tfong4386@gmail.com>
2019-03-11 12:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function happyHolidays() {
   return `Happy holidays!`
 }
 function happyHolidaysTo(name) {
-  return `${name}`
-  
+  return `Happy holidays, ${name}`
 }
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`}
