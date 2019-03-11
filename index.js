e088b5821e0a62b194c83d2ccc0a542714c156fa
tfong4386 <tfong4386@gmail.com>
2019-03-11 12:28:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays() {
+  return 'Happy holidays!';
+}
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}`}
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(holday, days) {
+  return `It's ${days} days until ${holidays}!`}
+
